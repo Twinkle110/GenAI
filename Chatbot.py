e738f25b5c6bc99b6613ffa86767ff6c919ba493
Twinkle Pardeshi <65865151+Twinkle110@users.noreply.github.com>
2024-04-10 17:00:05 +0530

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -81,7 +81,12 @@ def app():
             message(msg.content, is_user=True, key=str(i) + '_user')
         else:
             message(msg.content, is_user=False, key=str(i) + '_ai')
+            copy_button_id = f"copy_button_{i}"
+            if st.button("Copy", key=copy_button_id):
+               copy_to_clipboard(msg.content)
 
+def copy_to_clipboard(content):
+    pyperclip.copy(content)
 
 if __name__ == '__app__':
     app()
