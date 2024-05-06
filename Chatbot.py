291730631c47aad24865ff675e79ab44a9fd50a6
Twinkle Pardeshi <65865151+Twinkle110@users.noreply.github.com>
2024-05-06 11:58:49 +0530

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -4,6 +4,7 @@ import streamlit as st
 from streamlit_chat import message
 from dotenv import load_dotenv
 import os
+import  pyperclip
 
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import (
@@ -48,6 +49,43 @@ def app():
         """, unsafe_allow_html=True
     )
 
+    def message(msg, is_user=False, key=None):
+     if is_user:
+        avatar_url = "https://api.dicebear.com/8.x/fun-emoji/svg?seed=Simon"  # Replace with the URL of the user's avatar image
+        st.markdown(f'<div class="message user-message" key="{key}"><img src="{avatar_url}" class="avatar">{msg}</div>', unsafe_allow_html=True)
+     else:
+        avatar_url = "https://api.dicebear.com/8.x/bottts/svg"  # Replace with the URL of the AI's avatar image
+        st.markdown(f'<div class="message ai-message" key="{key}"><img src="{avatar_url}" class="avatar">{msg}</div>', unsafe_allow_html=True)
+
+
+    st.markdown("""
+    <style>
+    .message {
+        padding: 10px 10px;
+        margin: 10px 0;
+        border-radius: 10px;
+        max-width: 100%;
+    }
+    .user-message {
+        background-color: #2b313e; /* Adjust color as needed */
+        color: white; /* Adjust text color as needed */
+        text-align: left; /* Always align user messages to the left */
+    }
+    .ai-message {
+        background-color: #475063; /* Adjust color as needed */
+        color: white; /* Adjust text color as needed */
+        text-align: left; /* Align AI messages to the right */
+    }
+    .avatar {
+        width: 30px; /* Adjust avatar width as needed */
+        height: 30px; /* Adjust avatar height as needed */
+        margin-right: 10px; /* Adjust spacing between avatar and message */
+        border-radius: 50%; /* Make avatar circular */
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+
     chat = ChatOpenAI(model="gpt-4-turbo",temperature=0)
 
     # initialize message history
@@ -73,6 +111,7 @@ def app():
                 response = chat(st.session_state.messages)
               st.session_state.messages.append(
                 AIMessage(content=response.content))
+              
 
     # display message history
     messages = st.session_state.get('messages', [])
@@ -81,7 +120,17 @@ def app():
             message(msg.content, is_user=True, key=str(i) + '_user')
         else:
             message(msg.content, is_user=False, key=str(i) + '_ai')
-           
+    
+   
 
-if __name__ == '__app__':
+    # st.button("End", key=123)
+    if len(messages) > 1:
+        st.button("End", key=123)
+        # st.button("End")
+    # copy_to_clipboard() 
+
+    # st.button("End", key=123)
+
+
+if __name__ == '__main__':
     app()
