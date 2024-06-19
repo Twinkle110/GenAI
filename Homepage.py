78da96a5085d06346722f2a432a5100fe62e2771
Twinkle Pardeshi <65865151+Twinkle110@users.noreply.github.com>
2024-06-19 13:01:29 +0530

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -61,6 +61,8 @@ def run():
         ChatwithDocuments.app()
     if app == 'Performance Evaluation':
         PEFinal.app()
+    if app == 'Interview Analyser':
+        FinalInterview.app()
     elif app == 'PDF GPT':
         PDF_GPT.app()
 
